7a7093bb202d2e2eec665e63c04f01ec90e98ea3
Erik Scarcela Araujo <earaujo@albacore.com.br>
2026-08-21 17:51:57 -0300

    feat: Farming page initial view

---
+++ Macro/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Macro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFarming_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Parado");
+        }
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Iniciado!");
+        }
+    }
+}
